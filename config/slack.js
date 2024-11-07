4b05b59dd60927f3c4699ad314c7bde1a6b11d6f
Lakshya Kumar Singh <lakshya@gkmit.co>
2024-11-07 13:30:30 +0530

    feat: config slack api

--- config/slack.js
+++ config/slack.js
@@ -1,7 +1,11 @@
-const { WebClient } = require('@slack/web-api');
+const { WebClient, LogLevel } = require('@slack/web-api');
+require('dotenv').config();
 
 const token = process.env.SLACK_BOT_TOKEN;
 
-const slackClient = new WebClient(token);
+console.log(token);
+const slackClient = new WebClient(token, {
+	logLevel: LogLevel.DEBUG
+});
 
 module.exports = slackClient;
